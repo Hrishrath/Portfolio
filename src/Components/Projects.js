e265f1bd245bcd7495fb1a4970c6280a5947b713
Hrishrath <rish.rathore9@gmail.com>
2021-02-22 16:20:32 +0530

    components added

--- src/Components/Projects.js
+++ src/Components/Projects.js
@@ -11,10 +11,11 @@ const Projects = () => {
     </div>
     <div className="row">
       <ProjCard 
-        src = "https://colorlib.com/wp/wp-content/uploads/sites/2/jevelin-photo-gallery-website-template.jpg"
-        title = "Nanovo"
-        subtitle = "Design e-shop"
-        tags = "FRONTEND  —  BACKEND"
+        src = "https://lh3.googleusercontent.com/6GK8w8B-tzJvSZko44xzTyqEjhy6o4x8EDY8UL6AyBdPkaaPPE5AFtkGtrBR1frAifowQrWkvtdRl-RMxVGSQIYuR5SUvhtRImIrJ-e0VFDd6VPbHkO538zuJZNIc3qENAe5Iq-Wq61rCvN6BCD2SUWW5jJ3IAbtpoTjLSfKcM486uOPgxapH_e8-ULDlR9Lhvm6qVv9BYwN17ymbaILG-Z9YAlWNpP3N6_nSe2iLqLpIQ5p7RnamIaJmsSxDXDhhKsouDhX0jdDwn5zEDTxhJTLLjIQpTuc96S7FRxgBZHqZ10VKTF_q321bHzeSdJbimdFVAytcVUSWW53oWJAXYLvF1ysMM_0SUmnQhhvomecr-6NuGZBb6Zu-8hsL-1fBfVceo7wrsFDJY75utBXVrB6yskhJPiPY2e23wwDVsPIDnI0MyYOa09YHmgTzWYtXrxDy1Lso1MeqXcNHRp15kDQXyszGnjopFMtEdOD_4gI8EXVdkURxWfGhKmgwJVmUPlu_VTue4RESwmYMay9SXQVqKWAI__zJJhfvnUMLqXlEoNcecfdL0VZIEjrD5h-y00iD7-qDb75IyeIzRt5_8L71GtQIjO857HKip0-TCPU8czjKGQmzJruElawNopkqwtZBuDeJP8QwBAIJtlHF8FbDAqTy-9B-zS14QGgUox5HASJR0NNagKOa4RJeFM=w1349-h605-no?authuser=0"
+        title = "FruitLust"
+        subtitle = "Fruits & juices e-shop"
+        url = "http://www.fruitlust.store"
+        tags = "FRONTEND"
         />
       <ProjCard 
         src = "https://webdesigntips.blog/wp-content/uploads/2018/11/Top-21-Gallery-HTML5-Website-Templates-To-Showcase-Your-Stunning-Images-2018.jpg"
--- src/Components/Shared/ProjCard.js
+++ src/Components/Shared/ProjCard.js
@@ -4,9 +4,9 @@ const ProjCard = (props) => {
   return (
     <div className="col-12 col-md-4">
         <a className="" href="#modal-project-1" data-toggle="modal">
-            <img className="card-img-top" src={props.src} alt="Nanovo"/>
+            <img className="card-img-top" onClick = {() => {window.location.href = props.url}} src={props.src} alt="Nanovo"/>
             <div className="card-body">
-                <h3 className="card-title">{props.title}</h3>
+                <h3 className="card-title" onClick = {() => {window.location.href = props.url}}>{props.title}</h3>
                 <h4 className="card-subtitle">{props.subtitle}</h4>
                 <p className="card-tags">{props.tags}</p>
             </div>
